05bf51bca09644830cf9abe4ae9fee49e67996eb
adrianmurage <adrianmurage21@gmail.com>
2021-04-29 21:57:47 +0300

    start working on the logic for the calculator

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// the idea here is first to have the behavior of clicking a number implemented
 
+const calculatorDiv = document.querySelector('.calculator')
+
+calculatorDiv.addEventListener('click', function(event){
+    // console.log(event.target.querySelector('.number'))
+    if(event.target.tagName === 'BUTTON' && event.target.matches('.number')) {
+        console.log(`you clicked ${event.target.innerText}`)
+    }
+})
