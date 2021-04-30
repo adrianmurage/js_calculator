1251b575b9a1d576d48e2edb8d36791104a0e329
adrianmurage <adrianmurage21@gmail.com>
2021-04-30 22:09:35 +0300

    add logic to clear the display

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // the idea here is first to have the behavior of clicking a number implemented
 
 const calculatorDiv = document.querySelector('.calculator')
+const display = document.querySelector('.display')
 
-calculatorDiv.addEventListener('click', function(event){
-    // console.log(event.target.querySelector('.number'))
-    if(event.target.tagName === 'BUTTON' && event.target.matches('.number')) {
-        console.log(`you clicked ${event.target.innerText}`)
+calculatorDiv.addEventListener('click', function (event) {
+    if (event.target.matches('.clear-button')) {
+        display.innerText = '0';
     }
-})
+    
+    if (event.target.matches('.number')) {
+        if (display.innerText === '0'){
+            display.innerText = ''
+        }
+        display.innerText += event.target.innerText
+    }
+});
