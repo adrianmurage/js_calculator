779b9996876190d166a0ddd1e5b149177888b6b3
adrianmurage <adrianmurage21@gmail.com>
2021-05-04 23:17:01 +0300

    make changes to the calculator logic
    after struggling with this for a LONG ass time I think I'm done
    I haven't tested this thoroughly but I'm commiting this
    I'll go over it tomorrow before concluding with it and moving on

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const calculatorDiv = document.querySelector(".calculator");
 const display = document.querySelector(".display");
 
 let runningTotal = 0;
-let lastValue = null;
+let buffer = "0";
 let operator = null;
+
+calculatorDiv.addEventListener("click", function (event) {
+  if (event.target.matches(".clear-button")) {
+    clearDisplay();
+  } else if (event.target.matches(".number")) {
+    handleNumbers(event);
+  } else if (event.target.matches(".operator")) {
+    handleOperators(event);
+  } else if (event.target.matches(".equals")) {
+    displayResult(event);
+  }
+});
 
 function clearDisplay() {
   display.innerText = "0";
-  lastValue = null;
+  buffer = null;
   operator = null;
 }
 
@@ -20,45 +32,42 @@ function handleNumbers(event) {
   display.innerText += event.target.innerText;
 }
 
-function runCalculations() {
-  switch (operator) {
-    case "÷":
-      runningTotal /= lastValue;
-      break;
-    case "×":
-      runningTotal *= lastValue;
-      break;
-    case "−":
-      runningTotal -= lastValue;
-      break;
-    case "+":
-      runningTotal += lastValue;
-      break;
-
-    default:
-        
-      break;
-  }
-  console.log(runningTotal);
-}
-
 function handleOperators(event) {
   if (operator === null) {
+    console.log(`click operator when ${operator}`);
     runningTotal = parseInt(display.innerText);
+    display.innerText = "0";
+    operator = event.target.innerText;
+    console.log(`operator set to ${operator}`);
+  } else {
+    buffer = parseInt(display.innerText);
+    runCalculations(buffer);
     operator = event.target.innerText;
     display.innerText = "0";
-  } else {
-    lastValue = parseInt(display.innerText);
-    console.log("here");
-    runCalculations();
   }
 }
-calculatorDiv.addEventListener("click", function (event) {
-  if (event.target.matches(".clear-button")) {
-    clearDisplay();
-  } else if (event.target.matches(".number")) {
-    handleNumbers(event);
-  } else if (event.target.matches(".operator")) {
-    handleOperators(event);
+
+function runCalculations(buffer) {
+  switch (operator) {
+    case "÷":
+      runningTotal /= buffer;
+      break;
+    case "×":
+      runningTotal *= buffer;
+      break;
+    case "−":
+      runningTotal -= buffer;
+      break;
+    case "+":
+      runningTotal += buffer;
+      break;
   }
-});
+}
+
+function displayResult(event) {
+  handleOperators(event);
+  display.innerText = runningTotal;
+  runningTotal = 0;
+  buffer = "0";
+  operator = null;
+}
